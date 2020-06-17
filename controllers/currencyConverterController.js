9a2ad51a19d1ef67f3427f34fa9b87cfdd515ba1
Naïm <naiim.chatti@gmail.com>
2020-06-17 10:57:08 +0200

    add symbols, history and send errors

--- controllers/currencyConverterController.js
+++ controllers/currencyConverterController.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
+const moment = require('moment'); // require
 const request = require('request-promise-native');
 
 exports.getConvertedCurrency = async (req, res) => {
   const { base_currency, value, quote_currency } = req.query;
 
   const options = {
-    uri: `http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`,
+    uri: `${process.env.API_URL}latest?access_key=${process.env.API_KEY}`,
     json: true,
   };
 
@@ -17,6 +18,48 @@ exports.getConvertedCurrency = async (req, res) => {
     const convertedCurrency = (value * rates[quote_currency]) / rates[base_currency];
     res.json(convertedCurrency);
   } catch (error) {
-    console.error(error);
+    res.status(err.status || 500);
+    res.json(error);
   }
 };
+
+exports.getSymbols = async (req, res) => {
+  const options = {
+    uri: `${process.env.API_URL}symbols?access_key=${process.env.API_KEY}`,
+    json: true,
+  };
+
+  try {
+    const response = await request(options);
+    if (!response.success) return;
+
+    res.json(response.symbols);
+  } catch (error) {
+    res.status(err.status || 500);
+    res.json(error);
+  }
+};
+
+exports.getHistory = async (req, res) => {
+  const history = {};
+
+  try {
+    for (let i = 0; i < 5; i++) {
+      const date = moment().subtract(i, 'd').format('YYYY-MM-DD');
+      const options = {
+        uri: `${process.env.API_URL}${date}?access_key=${process.env.API_KEY}`,
+        json: true,
+      };
+
+      if (history[`${date}`]) continue;
+      const response = await request(options)
+      history[`${date}`] = response.rates;
+    }
+
+    res.json(history)
+  } catch (error) {
+    res.status(err.status || 500);
+    res.json(error);
+  }
+};
+
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const currencyConverterController = require('../controllers/currencyConverterController');
 
 router.get('/convert', currencyConverterController.getConvertedCurrency);
+router.get('/symbols', currencyConverterController.getSymbols);
+router.get('/history', currencyConverterController.getHistory);
 
 module.exports = router;
